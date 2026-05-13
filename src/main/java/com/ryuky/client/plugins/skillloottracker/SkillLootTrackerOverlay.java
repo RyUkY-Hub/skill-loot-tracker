d5a6ce5cfa9929cdf62708141a08bc56544d8ff3
RyUkY-Hub <daviddavies9897@gmail.com>
2026-05-13 15:35:09 -0400

    Update SkillLootTrackerOverlay.java

--- src/main/java/com/ryuky/client/plugins/skillloottracker/SkillLootTrackerOverlay.java
+++ src/main/java/com/ryuky/client/plugins/skillloottracker/SkillLootTrackerOverlay.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026, RyUkY <realmftalk420@gmail.com>
+ * Copyright (c) 2026, RyUkY-Hub <realmftalk420@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
